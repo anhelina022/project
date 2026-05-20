5c05ba2d7664bdf2aab77bf1bf6cabad431db552
Ангеліна Бебешко <angelinabebesko05@gmail.com>
2026-05-20 13:01:34 +0100

    add commigt

---
+++ small.project/src/main.js
@@ -0,0 +1,13 @@
+let ul = document.querySelector(".gallery");
+let btn = document.querySelector(".btn");
+let form = document.querySelector("#search-form")
+
+let page = 1;
+let perPage = 12;
+
+form.addEventListener("submit", onSearch);
+async function onSearch(event) {
+  try {
+    
+  }
+}
